46746c98cbb234a31aabb7008aa4cc7d4e0d713e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-02 20:16:03 -0700

    Add function to check that number input fields cannot contain the letter 'e'

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ var startActivityBtn = document.querySelector('#start-activity-button');
 var numbersInput = document.querySelector('.section-activity-time-input');
 
 window.addEventListener('load', disableStartActibityBtn);
-numbersInput.addEventListener('input', checkInputForNumbers);
-
+numbersInput.addEventListener('input', checkInputForNumbersOnly);
 
 function disableStartActibityBtn() {
   startActivityBtn.disabled = true;
 }
 
-function checkInputForNumbers() {
-  
+function checkInputForNumbersOnly() {
+  var minutes = document.querySelector('#minutes');
+  var seconds = document.querySelector('#seconds');
+
+  minutes.addEventListener('keyup', checkForLetterE);
+  seconds.addEventListener('keyup', checkForLetterE);
 }
 
-//make sure the time inputs can only accept numbers
-//adventlistener for an input
-//target input-erro-message to display error if user inputs a letter (even the letter "e")
+function checkForLetterE(event) {
+  var errorMessage = document.querySelectorAll('.input-error-message');
+
+  if (event.keyCode === 69) {
+    event.target.nextElementSibling.innerText = 'Numbers only please!';
+  } else {
+    event.target.nextElementSibling.innerText = '';
+  }
+}
+
 
 
 
